0dcbcb77fb18c55add2786ba71cc269ec278d646
PhocianTheGood <photiongames@gmail.com>
2019-12-21 02:16:31 -0800

    exercise adding fixed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const shortid = require('shortid');
+const moment = require('moment');
 require('dotenv').config();
 
 mongoose.connect(process.env.MLAB_URI, function(err, db) {
@@ -42,37 +43,38 @@ var userSchema = new Schema({
 });
 
 var exerciseSchema = new Schema({
-  userId: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true
-  },
-  date: Date,
-  duration: Number,
-  description: String,
+  username: String,
+	userId: {
+		type: String,
+		ref: 'User',
+		required: true,
+	},
+	date: Date,
+	duration: Number,
+	description: String,
 });
 
 var User = mongoose.model('User', userSchema);
 var Exercise = mongoose.model('Exercise', exerciseSchema);
 
-// 1. I can create a user by posting form data username to /api/exercise/new-user 
-// and returned will be an object with username and _id.
-// Example: SylQ9Ot0S
+// Done // 1. I can create a user by posting form data username to /api/exercise/new-user 
+// Done // and returned will be an object with username and _id.
 app.post('/api/exercise/new-user', function(req, res) {
   var username = req.body.username;
   var newUser = new User({
     username
   });
   newUser.save(function(err, user) {
-    // duplicate username: code == 11000
-    if (err) return res.send(err);
+    if (err){
+      if (err.code == 11000) return res.send("username already taken");
+      return res.send(err);
+    } 
     res.send(user);
   });
-  //console.log(req.body);
 });
 
-// 2. I can get an array of all users by getting api/exercise/users 
-// with the same info as when creating a user.
+// Done // 2. I can get an array of all users by getting api/exercise/users 
+// Done // with the same info as when creating a user.
 app.get('/api/exercise/users', function(req, res) {
   User.find({}, function(err, users) {
     if (err) return console.error(err);
@@ -80,37 +82,49 @@ app.get('/api/exercise/users', function(req, res) {
   });
 });
 
+// UserId: ThWR8sEV
+
 // 3. I can add an exercise to any user by posting form data userId(_id),
 // description, duration, and optionally date to /api/exercise/add.
-// If no date supplied it will use current date. 
-// Returned will be the user object with also with the exercise fields added.
+// Test // If no date supplied it will use current date. 
+// Test // Returned will be the user object with the exercise fields added.
 // Return Example: {"username":"usernamer","description":"new exedrcise","duration":10,"_id":"SylQ9Ot0S","date":"Thu Dec 19 2019"}
 app.post('/api/exercise/add', function(req, res) {
   //{"userId":"123","description":"asd","duration":"11","date":""}
   let id = req.body.userId;
   User.findById(id, function(err, user) {
     if (err) return res.send('User not found');
+    let dateRegex = /[0-9]{4}\-[0-9]{2}\-[0-9]{1,2}/;
+    let inputDate = new Date(req.body.date);
+    let dateIsValid = !isNaN(inputDate.getTime());
+    console.log(inputDate, dateIsValid, Date.now());
     let newExercise = new Exercise({
+      username: user.username,
       userId: user._id,
-      date: req.body.date || Date.now,
+      date: "",
       duration: req.body.duration,
       description: req.body.description,
     });
     newExercise.save(function(err, savedExercise) {
       if (err) return console.log(err);
-      res.send(user, savedExercise);
+      res.json(savedExercise);
     });
   });
 });
 
 //4. I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id).
 // Return will be the user object with added array log and count (total exercise count).
-
-
 //5. I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit.
 // (Date format yyyy-mm-dd, limit = int)
+app.get('/api/log', function(req, res) {
+  let userId = req.query.userId;
+  let from = req.query.from;
+  let to = req.query.to;
+  res.send({ userId, from, to });
+});
 
-// end my code
+
+/* --- End my code --- */
 
 
 // Not found middleware
